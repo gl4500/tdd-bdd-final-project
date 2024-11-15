019b551e626303e1265bb55353fccee98c7b13fa
gl4500 <74549635+gl4500@users.noreply.github.com>
2024-11-15 17:09:55 -0600

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -188,7 +188,7 @@ class TestProductModel(unittest.TestCase):
         for product in found:
             self.assertEqual(product.available, available)
 
-    def test_find_by_availability(self):
+    def test_find_by_category(self):
         """It should Find Products by Category"""
         products = ProductFactory.create_batch(10)
         # Use a for loop to iterate over the products list and call the create().
@@ -205,3 +205,15 @@ class TestProductModel(unittest.TestCase):
         # Use a for loop to iterate over the found products.
         for product in found:
             self.assertEqual(product.category, category)
+    
+    def test_find_by_availability(self):
+        """It should Find Products by Availability"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        available = products[0].available
+        count = len([product for product in products if product.available == available])
+        found = Product.find_by_availability(available)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.available, available)
